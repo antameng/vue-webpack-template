ecdf556a5974ea1633b97897025803fbe0baf761
aimeng <671608311@qq.com>
2024-06-03 00:17:37 +0800

    测试分包

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,7 @@ import AutoImport from 'unplugin-auto-import/webpack';
 import Components from 'unplugin-vue-components/webpack';
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const config: Configuration = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -60,7 +60,38 @@ const config: Configuration = {
       dts: 'src/auto-imports.d.ts',
       resolvers: [ElementPlusResolver()]
     })
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          filename: 'libs.js',
+          priority: 10,
+          chunks: 'initial', // 只打包最初依赖的第三方 设置 chunks 为 'initial' 时，Webpack 只会拆分那些同步导入的代码块。
+          minChunks: 1
+        },
+        commons: {
+          name: 'chunk-commons',
+          test: resolve('src'), // can customize your rules
+          minChunks: 3, //  minimum common number
+          priority: 5,
+          reuseExistingChunk: true
+        },
+        elementUI: {
+          name: 'chunk-elementUI',
+          test: /[\\/]node_modules[\\/]_?element-plus(.*)/,  // 为了适应CNPM
+          priority: 20, // 重量需要大于lib和app，否则将被打包到lib或app中
+        }
+      }
+    }
+  }
+}
+
+function resolve(dir: any) {
+  return path.join(__dirname, dir)
 }
 
 export default config
